c83453554ce731792d2e7e7771ea8373a73bdea9
Gwe2B <gwenael2b@live.fr>
2021-04-28 11:14:53 +0200

    Add of translations

---
+++ Assets/Scripts/TranslateText.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public sealed class TranslateText : MonoBehaviour {
+    [SerializeField]
+    private string m_Key = null;
+
+    private void Start() {
+        var text = GetComponent<Text>();
+        text.text = Translation.Get(m_Key != string.Empty ? m_Key : text.text);
+        Destroy(this);
+    }
+}
---
+++ Assets/Scripts/Translation.cs
@@ -0,0 +1,81 @@
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class Translation : MonoBehaviour {
+    public static readonly SystemLanguage[] LANGUAGES = {
+        SystemLanguage.English, SystemLanguage.French
+    };
+
+    private static Dictionary<string, string> trad = null;
+
+#if UNITY_EDITOR
+    private static bool d_OverrideLanguage = false;
+    private static SystemLanguage d_Language = SystemLanguage.English;
+#endif
+
+    private static void CheckInstance() {
+        // Already Initialized ?
+        if(trad != null) { return; }
+
+        trad = new Dictionary<string, string>();
+        var lang = Application.systemLanguage;
+
+#if UNITY_EDITOR
+        // Override the current language for testing purpose.
+        if (d_OverrideLanguage) { lang = d_Language; }
+#endif
+        // Check if the current language is supported.
+        // Otherwise use the first language as default.
+        if (Array.IndexOf<SystemLanguage>(LANGUAGES, lang) == -1)
+        { lang = LANGUAGES[0]; }
+
+        // Load and parse the translation file from the Resources folder.
+        var data = Resources.Load<TextAsset>($"Lang/{lang}");
+        if (data != null) { ParseFile(data.text); }
+    }
+
+    // Returns the translation for this key.
+    public static string Get(string key)
+    {
+        CheckInstance();
+        if (trad.ContainsKey(key)) { return trad[key]; }
+
+#if UNITY_EDITOR
+        Debug.Log($"The key {key} is missing");
+#endif
+
+        return key;
+    }
+
+    public static void ParseFile(string data)
+    {
+        using (var stream = new StringReader(data)) {
+            var line = stream.ReadLine();
+            var temp = new string[2];
+            var key = string.Empty;
+            var value = string.Empty;
+
+            while (line != null) {
+                if (line.StartsWith(";") || line.StartsWith("[")) {
+                    line = stream.ReadLine();
+                    continue;
+                }
+
+                temp = line.Split('=');
+                if (temp.Length == 2) {
+                    key = temp[0].Trim();
+                    value = temp[1].Trim();
+
+                    if (value == string.Empty) { continue; }
+                    if (trad.ContainsKey(key)) { trad[key] = value; }
+                    else { trad.Add(key, value); }
+                }
+                
+                line = stream.ReadLine();
+            }
+        }
+    }
+}
