6c32b4d73707ec2a9567353b46ae6c6bb9f25dc7
MarionCalpena <marioncalpe2a@gmail.com>
2021-04-30 12:05:29 +0200

    modif scriptshot ajout weapon

--- Assets/Scripts/ShotScript.cs
+++ Assets/Scripts/ShotScript.cs
@@ -1,8 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ShotScript : MonoBehaviour {
+/// <summary>
+/// Comportement des tirs
+/// </summary>
+public class ShotScript : MonoBehaviour
+{
+    // 1 - Designer variables
+
+    /// <summary>
+    /// Points de dégâts infligés
+    /// </summary>
     public int damage = 1;
+
+    /// <summary>
+    /// Projectile ami ou ennemi ?
+    /// </summary>
     public bool isEnemyShot = false;
+
+    void Start()
+    {
+        // 2 - Destruction programmée
+        Destroy(gameObject, 20); // 20sec
+    }
 }
